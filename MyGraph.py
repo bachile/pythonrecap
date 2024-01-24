795c836f220e35b14432a660ba06c4d52c16165f
Brando <brando_lezzana1@baylor.edu>
2024-01-24 14:41:21 -0600

    updated gitignore and print statement and comment lines (notes)

--- MyGraph.py
+++ MyGraph.py
@@ -1,3 +1,22 @@
+# Step 1
+# Create virtual environment
+# mac - python 3 -m venv _____
+# pc - py -3 -m venv _____
+
+# Step 2
+# Activate virtual environment
+# mac - source myvenv/bin/activate
+# pc - myvenv\scripts\activate
+
+# Step 3
+# Install 3rd party library/module
+# pip3 install {library} (in terminal)
+
+# Git config (for new account)
+# git config --global user.name "Brando"
+# git config --global user.email "brando_lezzana1@baylor.edu"
+# (in terminal)
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,4 +25,5 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
